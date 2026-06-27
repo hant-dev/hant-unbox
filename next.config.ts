1986823fc7cae9ba8e158c54a7abfc59dc02b414
ptminngoc <84673662+ptminngoc@users.noreply.github.com>
2026-06-27 16:15:06 +0700

    FIX

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',  // <-- THÊM DÒNG NÀY
+  images: {
+    unoptimized: true, // <-- THÊM DÒNG NÀY
+  },
+  trailingSlash: true, // <-- THÊM DÒNG NÀY
+  turbopack: {
+    root: process.cwd(),
+  },
 };
 
 export default nextConfig;
